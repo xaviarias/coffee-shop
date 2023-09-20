d6ff1820e677554a6e61a8233012d06ba5fa9e46
Xavier Arias <xavi.arias.segui@gmail.com>
2023-09-21 00:48:36 +0200

    Rename class for consistency

--- application/src/main/java/com/coffeeshop/application/service/CoffeeShopServiceImpl.java
+++ application/src/main/java/com/coffeeshop/application/service/CoffeeShopServiceImpl.java
@@ -3,32 +3,32 @@ package com.coffeeshop.application.service;
 import com.coffeeshop.domain.model.Order;
 import com.coffeeshop.domain.persistence.ProductRepository;
 import com.coffeeshop.domain.service.CoffeeShopService;
-import com.coffeeshop.domain.service.Printer;
+import com.coffeeshop.domain.service.PrinterService;
 import com.coffeeshop.domain.service.PromotionService;
 
 public class CoffeeShopServiceImpl implements CoffeeShopService {
 
     private final ProductRepository productRepository;
     private final PromotionService promotionService;
-    private final Printer printer;
+    private final PrinterService printerService;
 
     public CoffeeShopServiceImpl(
             ProductRepository productRepository,
             PromotionService promotionService,
-            Printer printer) {
+            PrinterService printerService) {
         this.productRepository = productRepository;
         this.promotionService = promotionService;
-        this.printer = printer;
+        this.printerService = printerService;
     }
 
     public void printMenu() {
-        productRepository.findAll().forEach(printer::printProduct);
+        productRepository.findAll().forEach(printerService::printProduct);
     }
 
     public void printOrderReceipt(Order order) {
         promotionService.findPromotion(order).ifPresentOrElse(
-                promotion -> printer.printOrderReceipt(order.accept(promotion)),
-                () -> printer.printOrderReceipt(order)
+                promotion -> printerService.printOrderReceipt(order.accept(promotion)),
+                () -> printerService.printOrderReceipt(order)
         );
     }
 }
---
+++ infrastructure/src/main/java/com/coffeeshop/infra/service/ConsolePrinterService.java
@@ -0,0 +1,26 @@
+package com.coffeeshop.infra.service;
+
+import com.coffeeshop.domain.model.Order;
+import com.coffeeshop.domain.model.Product;
+import com.coffeeshop.domain.service.PrinterService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsolePrinterService implements PrinterService {
+
+    @Override
+    public void printProduct(final Product product) {
+        System.out.printf("%s\t%s\n", product.name(), product.price());
+    }
+
+    @Override
+    public void printOrderReceipt(Order order) {
+        order.items().forEach(item ->
+                System.out.printf("%d\t%s\t%s\n",
+                        item.quantity(),
+                        item.product().name(),
+                        item.product().price())
+        );
+        System.out.printf("Total: %s", order.total());
+    }
+}
